3791cf1b030a683e81c8583f0aa0e03c50619aff
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-02 09:27:42 +0200

    Habit Model

---
+++ MyProject.Api/Models/Habit.cs
@@ -0,0 +1,7 @@
+public class Habit
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+}
