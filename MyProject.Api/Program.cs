e07e3c9a932665dcc31e130ab142bc8d405bebb1
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-05 11:21:31 +0200

    Added Swagger

--- MyProject.Api/Program.cs
+++ MyProject.Api/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.");
-
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -16,8 +16,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "My API");
+    });
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
 
