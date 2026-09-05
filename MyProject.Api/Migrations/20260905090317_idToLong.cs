435cb41277b161b0ea117008e1876d9f0c9cef54
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-05 11:21:48 +0200

    auto migrations

---
+++ MyProject.Api/Migrations/20260905090317_idToLong.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyProject.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class idToLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
