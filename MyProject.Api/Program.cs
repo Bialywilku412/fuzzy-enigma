7e19fcc8c07b453ac6e39ff9e252e2c435b952fb
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-10 11:21:35 +0200

    Connection beetween frontend and backend

--- MyProject.Api/Program.cs
+++ MyProject.Api/Program.cs
@@ -6,11 +6,19 @@ builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(option =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+})
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=selfdev.db"));
 
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
