1cb24070f659722054c9a20a706c9254625dea92
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-11 12:05:37 +0200

    Begin of navigating to habit itself

--- my-project-frontend/src/HabitRow.tsx
+++ my-project-frontend/src/HabitRow.tsx
@@ -1,4 +1,12 @@
-function HabitRow({ name, category, description }){
+import { useNavigate } from "react-router-dom";
+
+function HabitRow({id, name, category, description }){
+    const navigate = useNavigate();
+
+    function navigateToHabit() {
+        navigate(`/habits/${id}`);
+    }
+
     return(
         <tr>
             <td>{name}</td>
