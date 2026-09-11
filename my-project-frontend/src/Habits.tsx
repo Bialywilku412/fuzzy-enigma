992e0624d4ee5a301e782d52726048f6e5cf57db
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-11 12:04:50 +0200

    fixed export

---
+++ my-project-frontend/src/Habits.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import AddHabitForm from "./AddHabitForm";
+import HabitRow from "./HabitRow";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+function Habits(){
+    const [habits, setHabits] = useState([]);
+
+    function fetchHabits(){
+        fetch(`${API_BASE_URL}/habit`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setHabits(data);
+        });
+    }
+
+    useEffect(() => {
+        fetchHabits();
+    }, []);
+
+    return (
+        <>
+        <table>
+            <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Description</th>
+            </tr>
+            {habits.map(habit => (
+            <HabitRow
+                key={habit.id}
+                id={habit.id}
+                name={habit.name}
+                category={habit.category}
+                description={habit.description}
+            />
+            ))}
+        </table>
+        <div>
+            <AddHabitForm
+            onHabitAdded = {fetchHabits}/>
+        </div>
+        </>
+    )
+}
+
+export default Habits;
