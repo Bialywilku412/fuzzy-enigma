ad5c7d53b443feb6cbf2d69f300186e183aa8d47
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-11 11:33:59 +0200

    fetch habits function and passing it to AddHabitForm

--- my-project-frontend/src/App.tsx
+++ my-project-frontend/src/App.tsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from "react";
+import AddHabitForm from "./AddHabitForm";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function App()
 {
   const [habits, setHabits] = useState([]);
 
-  useEffect(() => {
-    fetch(`${API_BASE_URL}/habit`)
+  function fetchHabits(){
+     fetch(`${API_BASE_URL}/habit`)
       .then(response => response.json())
       .then(data => {
         console.log(data);
         setHabits(data);
       });
+  }
+
+  useEffect(() => {
+    fetchHabits();
   }, []);
 
   return (
-    <ul>
+    <><ul>
       {habits.map(habit => (
         <li key={habit.id}>{habit.id} , {habit.name}</li>
       ))}
-    </ul>
+    </ul><div>
+        <AddHabitForm
+          onHabitAdded = {fetchHabits}/>
+      </div></>
   )
 }
 
