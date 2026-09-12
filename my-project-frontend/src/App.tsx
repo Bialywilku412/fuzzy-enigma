2511d29b2b002fc00266e87b9e6edac036506e50
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-12 09:25:41 +0200

    setting navbar in the app

--- my-project-frontend/src/App.tsx
+++ my-project-frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 import Habits from "./Habits"
+import Navbar from "./NavBar";
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function App()
 {
   return(
+    <>
+    <Navbar />
     <BrowserRouter>
       <Routes>
         <Route element={<Habits />}>
           <Route index element={<Habits />} />
-          <Route path="missions" element={<Habits />} />
+          <Route path="habits" element={<Habits />} />
         </Route>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
