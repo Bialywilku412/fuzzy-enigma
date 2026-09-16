d94fa4090b2b02c61b3be54edd844703978b7348
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-16 09:47:27 +0200

    ToDoitems table for overview

--- my-project-frontend/src/ToDoList/TodoItemsTabel.tsx
+++ my-project-frontend/src/ToDoList/TodoItemsTabel.tsx
@@ -1,10 +1,26 @@
+import TodoItemRow from "./TodoItemRow";
 import styled from "styled-components";
 
+const StyledTable = styled.div`
+  display: grid;
+  gap: 8px;
+  grid-auto-rows: min-content;
+  min-height: 300px;
+`;
 
-const StyledTodoItemsTable = styled.div`
-    display gr
-`
-
-function todoItemsTable{
-
+function TodoItemsTable({ todoItems, onDeleteTodoItem, onUpdateTodoItem }) {
+    return(
+        <StyledTable>
+            {todoItems.map((data) => (
+                <TodoItemRow
+                    key={data.id}
+                    data={data}
+                    onDeleteTodoItem={onDeleteTodoItem}
+                    onUpdateTodoItem={onUpdateTodoItem}
+                />
+            ))}
+        </StyledTable>
+    );
 }
+
+export default TodoItemsTable;
