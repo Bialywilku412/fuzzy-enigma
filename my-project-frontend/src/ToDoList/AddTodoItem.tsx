46f05386923b9e5d8d74694e785b8c71a432f600
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-16 09:46:39 +0200

    fix

--- my-project-frontend/src/ToDoList/AddTodoItem.tsx
+++ my-project-frontend/src/ToDoList/AddTodoItem.tsx
@@ -1,28 +1,53 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-function AddTodoItemForm({ onTodoItemAdded }){
+type AddTodoItemFormProps = {
+    onTodoItemAdded: () => void;
+};
+
+function AddTodoItemForm({ onTodoItemAdded }: AddTodoItemFormProps){
     const [newTodoItem, setNewTodoItem] = useState({
         name: "",
         date: new Date(),
         isDone: false
     });
 
-    const handleSubmit((e) =>{
+    function handleSubmit(e: FormEvent<HTMLFormElement>)
+    {
         e.preventDefault();
 
-        fetch(`${API_BASE_URL}/todo`, {
+        fetch(`${API_BASE_URL}/TodoItem`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(newTodoItem)
         })
-        .then(response => response.json())
-        .then(createdTodoItem =>{
-            onTodoItemAdded(createdTodoItem)
-        })
-    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to add todo item: ${response.status}`);
+            }
+
+            onTodoItemAdded();
+        });
+    };
+
+    return(
+        <>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <input
+                            type="text"
+                            value={newTodoItem.name}
+                            onChange={(e) => setNewTodoItem({ ...newTodoItem, name: e.target.value })}
+                            />
+                    </label>
+                    <input type="submit"></input>
+                </form>
+            </section>
+        </>
+    );
 }
 
 export default AddTodoItemForm;
