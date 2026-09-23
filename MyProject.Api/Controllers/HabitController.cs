dfffc18e44ff4885a6f9f69b21f83e8077f8736b
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-23 11:05:52 +0200

    auuthorize in controller

--- MyProject.Api/Controllers/HabitController.cs
+++ MyProject.Api/Controllers/HabitController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class HabitController : ControllerBase
 {
     private readonly AppDbContext _context;
--- MyProject.Api/Controllers/TodoItemController.cs
+++ MyProject.Api/Controllers/TodoItemController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class TodoItemController : ControllerBase
 {
     private readonly AppDbContext _context;
