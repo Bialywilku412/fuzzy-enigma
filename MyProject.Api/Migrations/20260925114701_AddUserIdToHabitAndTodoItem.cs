29fa18d9dc8317f5ad3afff18b408cab40504ab4
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-25 13:47:48 +0200

    db migration

---
+++ MyProject.Api/Migrations/20260925114701_AddUserIdToHabitAndTodoItem.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyProject.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToHabitAndTodoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "TodoItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Habits",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_UserId",
+                table: "TodoItems",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Habits_UserId",
+                table: "Habits",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Habits_Users_UserId",
+                table: "Habits",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Habits_Users_UserId",
+                table: "Habits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Habits_UserId",
+                table: "Habits");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Habits");
+        }
+    }
+}
