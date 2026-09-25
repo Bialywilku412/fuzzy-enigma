9b2ff1fdfbe208deb96c87b775000874c985a426
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-25 13:48:24 +0200

    user and userId property added

--- MyProject.Api/Models/Habit.cs
+++ MyProject.Api/Models/Habit.cs
@@ -4,4 +4,6 @@ public class Habit
     public string Name { get; set; }
     public string Category { get; set; }
     public string Description { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
 }
