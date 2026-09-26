ffab80e3d07c3d02d26b3cef17fc72f75def307a
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-09-26 10:36:46 +0200

    Revert "dto for updatinga a habit"
    This reverts commit 8c705133205d67921355816055481dbe4df21ba1.

--- MyProject.Api/DTOs/UpdateHabitRequest.cs
+++
@@ -1,6 +0,0 @@
-public class UpdateHabitRequest
-{
-    public string Name { get; set; }
-    public string Category { get; set; }
-    public string Description { get; set; }
-}
--- MyProject.Api/DTOs/UpdateTodoItemRequest.cs
+++
@@ -1,6 +0,0 @@
-public class UpdateTodoItemRequest()
-{
-    public string Name { get; set; }
-    public DateTime Date { get; set; }
-    public bool IsDone { get; set; }
-}
